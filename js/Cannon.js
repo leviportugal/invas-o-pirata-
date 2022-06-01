4e736ae9716bd36e5e6e94bc686217fe085f1188
leviportugal <102754345+leviportugal@users.noreply.github.com>
2022-06-01 13:07:19 -0300

    first commit

---
+++ js/Cannon.js
@@ -0,0 +1,18 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonimage = loadImage ("assets/canon.png")
+    this.cannonbaseimage = loadImage ("assets/cannonBase.png")
+  }
+  show (){
+    image (this.cannonbaseimage, 70, 20, 200, 200)
+    push ()
+    imageMode (CENTER)
+    image (this.cannonimage, this.x, this.y, this.width, this.height)
+    pop ()
+  }
+}
