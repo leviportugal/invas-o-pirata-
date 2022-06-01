68b44ad67928bcd6f72a4584b62be51eee0635cd
edwedja <edwedja_lima@hotmail.com>
2022-06-01 13:32:43 -0300

    aula 23

---
+++ js/CannonBall.js
@@ -0,0 +1,18 @@
+class CannonBall {
+    constructor(x,y){
+
+        this.r =30
+        this.body = Bodies.circle(x,y, this.r, {isStatic:true});
+        this.image = loadImage ("assets/cannonball.png")
+        World.add(world, this.body);
+    }
+
+    show (){
+        push ()
+        imageMode(CENTER)
+        image ( this.image, this.body.position.x, this.body.position.y,this.r, this.r  )
+        pop ()
+    }
+
+
+}
